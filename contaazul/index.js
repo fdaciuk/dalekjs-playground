2db0ac394b4e35497d204da5db68fc0c518cb147
Fernando Daciuk <f.daciuk@gmail.com>
2015-08-31 07:38:11 -0300

    Separate files to new tests folder

--- contaazul/index.js
+++ contaazul/index.js
@@ -1,14 +1,4 @@
+var login = require('./tests/login');
 module.exports = {
-  'Create new account at ContaAzul': function(test) {
-    test.open('https://app.contaazul.com')
-      .click('a[href="#modalCadastro"]')
-      .type('#contaazul-name', 'Fernando Daciuk')
-      .type('#contaazul-company', 'Empresa')
-      .type('#contaazul-phone', '(11) 1111-11111')
-      .type('#contaazul-email', new Date().getTime() + '@contaazul.com')
-      .type('#contaazul-pass', '123456')
-      .click('#contaazul-terms')
-      .screenshot('app-ca.png')
-      .done();
-  }
+  'Create new account at ContaAzul': login.createAccount
 };
