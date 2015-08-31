030323ed3489a834aaebc66652e3b912caffc51a
Fernando Daciuk <fernando@email.com>
2015-08-31 07:38:45 -0300

    Add tests folder

---
+++ contaazul/tests/login/index.js
@@ -0,0 +1,12 @@
+exports.createAccount = function createAccount(test) {
+  test.open('https://app.contaazul.com')
+    .click('a[href="#modalCadastro"]')
+    .type('#contaazul-name', 'Fernando Daciuk')
+    .type('#contaazul-company', 'Empresa')
+    .type('#contaazul-phone', '(11) 1111-11111')
+    .type('#contaazul-email', new Date().getTime() + '@contaazul.com')
+    .type('#contaazul-pass', '123456')
+    .click('#contaazul-terms')
+    .screenshot('./screenshots/:browser/app-ca_:timestamp.png')
+    .done();
+};
